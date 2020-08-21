48f6332fed3987fec8a5b9edf74af72ee31c4533
lukas <shenne@smail.uni-koeln.de>
2020-08-21 10:24:15 +0200

    update unit tests for `collect_from_directory`

--- test.py
+++ test.py
@@ -1,21 +1,43 @@
 import unittest
 from analyze_acoustics import *
 
-class AnalyzeAcousticsTests(unittest.TestCase):
+class CollectFromDirectoryTests(unittest.TestCase):
     
-    def test_collect_from_directory(self):
+    def test_invalid_type_input(self):
+        """
+        Do invalid type inputs return an error?
+        """
         
-        with self.assertRaises(TypeError):
-            collect_from_directory(20)
-        with self.assertRaises(TypeError):
-            collect_from_directory(None)
-        with self.assertRaises(TypeError):
-            collect_from_directory("")
-        with self.assertRaises(ValueError):
-            collect_from_directory("NotAValidPath")
+        for test_case in (20, None, "", True):
+            with self.subTest(test_case):
+                with self.assertRaises(TypeError):
+                    collect_from_directory(test_case)
+                    
+    def test_bad_string_input(self):
+        """
+        Do nonexistent paths as input return an error?
+        """
+        
+        for test_case in ("NotAValidPath", "another/kind/of/invalid", "also-2020ü&not_valid/"):
+            with self.subTest(test_case):
+                with self.assertRaises(ValueError):
+                    collect_from_directory(test_case)
+        
+    
+    def test_correct_output_type(self):
+        """
+        Is the method output is a dictionary?
+        """
         
         collection = collect_from_directory("test_material/")
         self.assertIsInstance(collection, dict)
+        
+    def test_correct_output_content(self):
+        """
+        Does example input return the correct output?
+        """
+        
+        collection = collect_from_directory("test_material/")
         self.assertIn("AH_95", collection.keys())
         self.assertIn("0018.TextGrid", collection["AH_95"])
         self.assertIn("0098.TextGrid", collection(["AH_95"]))
