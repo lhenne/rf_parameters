8f810ec1cf7aa66ed21c9d21413ae5168c62b52c
lukas <shenne@smail.uni-koeln.de>
2020-08-21 10:52:34 +0200

    fix bad assertion

--- test.py
+++ test.py
@@ -40,6 +40,6 @@ class CollectFromDirectoryTests(unittest.TestCase):
         collection = collect_from_directory("test_material/")
         self.assertIn("AH_95", collection.keys())
         self.assertIn("0018.TextGrid", collection["AH_95"])
-        self.assertIn("0098.TextGrid", collection(["AH_95"]))
+        self.assertIn("0098.TextGrid", collection["AH_95"])
         self.assertIn("0057.TextGrid", collection["AH_95"])
         self.assertNotIn("0018.wav", collection["AH_95"])
